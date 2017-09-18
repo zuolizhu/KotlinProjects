b4e5f7d2186bef1174129815f5077c8ff7f2b2de
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-09-18 11:02:16 -0400

    object type variable
    val can be assigned only once. but it is useful to use val type

--- Basic/src/com/zuolizhu/VariableDeclarations/Declarations.kt
+++ Basic/src/com/zuolizhu/VariableDeclarations/Declarations.kt
@@ -8,5 +8,21 @@ fun main(args: Array<String>) {
     var changeableNumber: Int
     changeableNumber = 12
     changeableNumber = 15
+
+    val employee1 = Employee("Zuoli Zhu", 1)
+    employee1.name = "ZL Z"
+    val employee2 : Employee
+    val number2 = 2
+
+    if(number > number2) {
+        employee2 = Employee("Bad Name", 200)
+    } else {
+        employee2 = Employee("Good Name", 100)
+    }
+
+    
 }
 
+class Employee(var name: String, val id: Int) {
+
+}
