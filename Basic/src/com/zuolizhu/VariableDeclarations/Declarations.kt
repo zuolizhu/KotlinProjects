fb79bcddc021ef24c14e375dc9dad6b868a15718
Zuoli Zhu <zuolizhu5960x@gmail.com>
2017-09-18 10:56:18 -0400

    difference between val and var
    val: same as final in java, cannot be change

---
+++ Basic/src/com/zuolizhu/VariableDeclarations/Declarations.kt
@@ -0,0 +1,12 @@
+package com.zuolizhu.VariableDeclarations
+
+fun main(args: Array<String>) {
+    val number = 25
+    val intergerNumber: Int
+    intergerNumber = 10
+
+    var changeableNumber: Int
+    changeableNumber = 12
+    changeableNumber = 15
+}
+
